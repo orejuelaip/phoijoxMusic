90cb867272401d363d666d81856fe2a0628d6367
Paul <orejuelaip@gmail.com>
2017-11-22 23:28:33 -0500

    Inicio de commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default{
+	appName:'platzimusic',
+	apiKey:'a5bbd74fffad70fb5bd9e27535bcc7c3',
+	secret:'59682d474c8ec1e939fdba71998e17c4',
+	registeredTo:'phoijox'
+}
+
+// Application name	platzimusic
+// API key	a5bbd74fffad70fb5bd9e27535bcc7c3
+// Shared secret	59682d474c8ec1e939fdba71998e17c4
+// Registered to	phoijox
